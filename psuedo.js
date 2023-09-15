58ad25e7d6158ea28bf058d6b647b6fe9424f5f8
James Spencer <e.spencer@kyspencers.net>
2023-09-15 19:02:29 +0000

    JS changes and scratchpad

---
+++ psuedo.js
@@ -0,0 +1,27 @@
+// START
+
+//Set our variables
+/*
+const currentTime =
+
+let selectYourTime =
+
+const isAlarmSet =  
+
+const setAlarmButton =
+*/
+
+/*
+ Create the functions
+
+SET the alarm
+
+WHEN alarm reaches time: alert (play "HEY! LISTEN!")
+
+RESET the alarm
+
+
+
+
+
+//END
